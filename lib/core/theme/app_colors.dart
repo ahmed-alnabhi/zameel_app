275dbefd24fd7f9df59ad983a2cede33b96cf99c
ahmed-alnabhi <ahmed2004.1436@gmail.com>
2025-05-12 01:10:59 +0300

    Add styling configuration

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6366F1);
+  static const Color secondaryColor = Color(0xFF1B1A1F);
+  static const Color primaryColorDark = Color(0xFF000000);
+  static const Color secondaryColorDark = Color(0xFF1B1A1F);
+  static const Color primaryLightColor = Color(0xFFF0F2F5);
+  static const Color primary50opacityColor = Color(0xffb1b2f8);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color backgroundColorDark = Color(0xFF000000);
+  static const Color textColorLightModePrimary = Color(0xFF1B1A1F);
+  static const Color textColorLightModeSecondary = Color(0xFF7A869A);
+  static const Color textColorDarkModePrimary = Color(0xFFF0F2F5);
+  static const Color textColorDarkModeSecondary = Color(0xFF7A869A);
+  static const Color textDisabledColor = Color(0xFFEDEDED);
+}
